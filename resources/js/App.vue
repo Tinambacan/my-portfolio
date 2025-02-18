9ffff224f0cbafd832bcac5399af9531f5ffb26d
joselito_jl <joselito.tinambacan@gmail.com>
2025-02-18 15:07:46 +0800

    Add laravel,vue,tailwind

---
+++ resources/js/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="app">
+      <h1 class="text-center hidden">Welcome to My Portfolio</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'App'
+  }
+  </script>
+  
+  <style scoped>
+  /* Your CSS styles here */
+  </style>
+  
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './resources/**/*.blade.php',
+        './resources/**/*.vue',
+        './resources/**/*.js',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    plugins: [],
+};
