4ab2a4f8ea21a3c49b59cbbe0206c7cc1ceed82d
joselito_jl <joselito.tinambacan@gmail.com>
2025-03-02 22:55:45 +0800

    fixed deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 import path from 'path';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/my-portfolio/' : '/',
+  base: '/my-portfolio/', // Replace with your repo name
   plugins: [
     laravel({
       input: ['resources/css/app.css', 'resources/js/app.js'],
@@ -18,4 +18,3 @@ export default defineConfig({
     },
   },
 });
-
