63ee35e7f951b0073c832b5580509c7dc96e9a84
joselito_jl <joselito.tinambacan@gmail.com>
2025-03-02 22:41:38 +0800

    Updated deployment setting

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import laravel from 'laravel-vite-plugin';
 import path from 'path';
 
 export default defineConfig({
+  base: '/my-portfolio/',
   plugins: [
     laravel({
       input: ['resources/css/app.css', 'resources/js/app.js'],
